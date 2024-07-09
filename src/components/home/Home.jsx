c2d7ba9ec58984699d4e3195c56205df1611999f
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-07-09 20:24:55 -0300

    feature: creacion del componente scrolldown modificacion de estilos

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./home.css";
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from '../home/Scrolldown';
 
 const Home = () => {
 return (
@@ -14,6 +15,8 @@ return (
 
                 <Data />
             </div>
+
+            <ScrollDown />
         </div>
     </section>
 )
