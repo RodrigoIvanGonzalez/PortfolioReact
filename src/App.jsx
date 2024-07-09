2f5f8b038d971f1d32491cd51095cfc3d7c70a0e
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-07-09 01:18:38 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
