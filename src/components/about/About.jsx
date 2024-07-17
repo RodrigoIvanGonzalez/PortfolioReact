39700cc8dc1fb07cdce432c5dbd2f73a873e0e51
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-07-16 23:22:50 -0300

    feature: creacion de componente contact con estilos

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./about.css";
-import AboutImg from "../../assets/about.jpg";
+import AboutImg from "../../assets/profile.jpg";
 import CV from "../../assets/John-Cv.pdf";
 import Info from './Info';
 
