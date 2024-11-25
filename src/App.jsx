309b9e405de8c1cdf98ca0573b2d8e5727efe0bc
RodrigoIvanGonzalez <r.ivann.gonzalezz@gmail.com>
2024-11-25 16:56:14 -0300

    finalizacion del componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import About from './components/about/About'
 import Skills from './components/skills/Skills'
+import Work from './/components/work/Work'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Scrollup from './components/scrollup/Scrollup'
@@ -18,6 +19,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Work />
         <Contact />
         
       </main>
---
+++ src/components/work/Data.jsx
@@ -0,0 +1,49 @@
+import Work1 from '../../assets/driverWeb.jpg'
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title:'web design',
+        category: 'web'
+    },
+    {
+        id: 2,
+        image: Work1,
+        title:'web design',
+        category: 'design'
+    },
+    {
+        id: 3,
+        image: Work1,
+        title:'web design',
+        category: 'web'
+    },
+    {
+        id: 4,
+        image: Work1,
+        title:'web design',
+        category: 'design'
+    },
+    {
+        id: 5,
+        image: Work1,
+        title:'web design',
+        category: 'app'
+    },
+
+]
+
+export const projectsNav = [ {
+    name: 'all',
+},
+{
+    name: 'web',
+},
+{
+    name: 'design',
+},
+{   
+    name: "app"
+}
+]
